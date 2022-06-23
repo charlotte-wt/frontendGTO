d10e2bb95817fc2050a2e90c3b4edc2214ffb7d0
charlotte-wt <charlottewee01@gmail.com>
2022-06-24 03:24:42 +0800

    added export default to import in rootReducer.js

--- src/components/Event/eventReducer.js
+++ src/components/Event/eventReducer.js
@@ -1,31 +1,33 @@
 const initialState = [
-    { id: 0, name: "Career Talk", email: "email@email.com", description: "This career talk is one where professionals will speak about their careers." },
-    { id: 1, name: "Networking Session", email: "test@test.com", description: "This networking session is for fellow members of GIRLSTIMEONLINE to get to know each other" },
+    { id: 0, username: "abc", email: "email@email.com",title: "Career Talk" ,description: "This career talk is one where professionals will speak about their careers." },
+    { id: 1, username: "def", email: "test@test.com", title:"Networking Session",description: "This networking session is for fellow members of GIRLSTIMEONLINE to get to know each other" },
 ];
   
 export const eventReducer = (state = initialState, action) => {
     switch (action.type) {
-        case "ADD_EVENT":
+      case "ADD_EVENT":
         state = [...state, action.payload];
         return state;
-        case "DELETE_EVENT":
+      case "DELETE_EVENT":
         const eventFilter = state.filter((event) =>
-            event.id === action.payload ? null : event
+          event.id === action.payload ? null : event
         );
         state = eventFilter;
         return state;
-        case "UPDATE_EVENT":
+      case "UPDATE_EVENT":
         const eventUpdate = state.filter((event) =>
-            event.id === action.payload.id
+          event.id === action.payload.id
             ? Object.assign(event, action.payload)
             : event
         );
         state = eventUpdate;
         return state;
-        case "RESET_EVENT":
-        state = [{ name: null, email: null, description: null }];
+      case "RESET_EVENT":
+        state = [{ username: null, email: null, title:null,description:null}];
         return state;
-        default:
+      default:
         return state;
     }
-};
+  };
+
+  export default eventReducer;
