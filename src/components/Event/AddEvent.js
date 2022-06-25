a6faab1eb76ff1a9879abf5b7365853545a0163d
charlotte-wt <charlottewee01@gmail.com>
2022-06-26 03:29:58 +0800

    updated new fe

--- src/components/Event/AddEvent.js
+++ src/components/Event/AddEvent.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createEvent } from "../../actions/events";
+import "./Event.css";
 
 const AddEvent = () => {
   const initialEventState = {
--- src/components/Event/EventList.js
+++ src/components/Event/EventList.js
@@ -1,141 +1,156 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  retrieveEvents,
-  findEventsByTitle,
-  deleteAllEvents,
+    retrieveEvents,
+    findEventsByTitle,
+    deleteAllEvents,
 } from "../../actions/events";
 import { Link } from "react-router-dom";
+import "./Event.css";
 
 const EventsList = () => {
-  const [currentEvent, setCurrentEvent] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(-1);
-  const [searchTitle, setSearchTitle] = useState("");
+    const [currentEvent, setCurrentEvent] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+    const [searchTitle, setSearchTitle] = useState("");
 
-  const events = useSelector(state => state.eventR);
-  const dispatch = useDispatch();
+    const events = useSelector(state => state.eventR);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(retrieveEvents());
-  }, []);
+    useEffect(() => {
+        dispatch(retrieveEvents());
+    }, []);
 
-  const onChangeSearchTitle = e => {
-    const searchTitle = e.target.value;
-    setSearchTitle(searchTitle);
-  };
+    const onChangeSearchTitle = e => {
+        const searchTitle = e.target.value;
+        setSearchTitle(searchTitle);
+    };
 
-  const refreshData = () => {
-    setCurrentEvent(null);
-    setCurrentIndex(-1);
-  };
+    const refreshData = () => {
+        setCurrentEvent(null);
+        setCurrentIndex(-1);
+    };
 
-  const setActiveEvent = (event, index) => {
-    setCurrentEvent(event);
-    setCurrentIndex(index);
-  };
+    const setActiveEvent = (event, index) => {
+        setCurrentEvent(event);
+        setCurrentIndex(index);
+    };
 
-  const removeAllEvents = () => {
-    dispatch(deleteAllEvents())
-      .then(response => {
-        console.log(response);
+    const removeAllEvents = () => {
+        dispatch(deleteAllEvents())
+            .then(response => {
+                console.log(response);
+                refreshData();
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const findByTitle = () => {
         refreshData();
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+        dispatch(findEventsByTitle(searchTitle));
+    };
 
-  const findByTitle = () => {
-    refreshData();
-    dispatch(findEventsByTitle(searchTitle));
-  };
+    return (
+        <div className="event-list-container">
+            <div className="list row">
+                <div className="col-md-8">
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by title"
+                            value={searchTitle}
+                            onChange={onChangeSearchTitle}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={findByTitle}
+                            >
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <h4>Events List</h4>
 
-  return (
-    <div className="list row">
-      <div className="col-md-8">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by title"
-            value={searchTitle}
-            onChange={onChangeSearchTitle}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              onClick={findByTitle}
-            >
-              Search
-            </button>
-          </div>
+                    <ul className="list-group">
+                        {events &&
+                            events.map((event, index) => (
+                                <li
+                                    className={
+                                        "list-group-item " + (index === currentIndex ? "active" : "")
+                                    }
+                                    onClick={() => setActiveEvent(event, index)}
+                                    key={index}
+                                >
+                                    {event.title}
+                                </li>
+                            ))}
+                    </ul>
+
+                    <button
+                        className="m-3 btn btn-sm btn-danger"
+                        onClick={removeAllEvents}
+                    >
+                        Remove All
+                    </button>
+                </div>
+                <div className="col-md-6">
+                    {currentEvent ? (
+                        <div>
+                            <h4>Event</h4>
+                            <div>
+                                <label>
+                                    <strong>Title:</strong>
+                                </label>{" "}
+                                {currentEvent.title}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Description:</strong>
+                                </label>{" "}
+                                {currentEvent.description}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Host Username:</strong>
+                                </label>{" "}
+                                {currentEvent.username}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Host Contact:</strong>
+                                </label>{" "}
+                                {currentEvent.email}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Status:</strong>
+                                </label>{" "}
+                                {currentEvent.published ? "Published" : "Pending"}
+                            </div>
+
+                            <Link
+                                to={"/events/" + currentEvent.id}
+                                className="badge badge-warning"
+                            >
+                                Edit
+                            </Link>
+                        </div>
+                    ) : (
+                        <div>
+                            <br />
+                            <p>Please click on a Event...</p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        <h4>Events List</h4>
-
-        <ul className="list-group">
-          {events &&
-            events.map((event, index) => (
-              <li
-                className={
-                  "list-group-item " + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActiveEvent(event, index)}
-                key={index}
-              >
-                {event.title}
-              </li>
-            ))}
-        </ul>
-
-        <button
-          className="m-3 btn btn-sm btn-danger"
-          onClick={removeAllEvents}
-        >
-          Remove All
-        </button>
-      </div>
-      <div className="col-md-6">
-        {currentEvent ? (
-          <div>
-            <h4>Event</h4>
-            <div>
-              <label>
-                <strong>Title:</strong>
-              </label>{" "}
-              {currentEvent.title}
-            </div>
-            <div>
-              <label>
-                <strong>Description:</strong>
-              </label>{" "}
-              {currentEvent.description}
-            </div>
-            <div>
-              <label>
-                <strong>Status:</strong>
-              </label>{" "}
-              {currentEvent.published ? "Published" : "Pending"}
-            </div>
-
-            <Link
-              to={"/events/" + currentEvent.id}
-              className="badge badge-warning"
-            >
-              Edit
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a Event...</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EventsList;
