6958b272f1101788f180745bfa82de445bda73b2
charlotte-wt <charlottewee01@gmail.com>
2022-07-01 00:08:25 +0800

    removed unused imports

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,13 +4,11 @@ import Header from "./Header";
 import Posts from "./Posts";
 import Sidebar from "./SideBar";
 import "./blog.css";
-import axios from "axios";
-import { useLocation } from "react-router";
+
 import BlogDataService from "../../services/blogService";
 
 export default function Blog() {
   const [posts, setPosts] = useState([]);
-  const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,7 +16,7 @@ export default function Blog() {
         const res = await BlogDataService.getAll();
         setPosts(res.data);
       } catch (err) {
-        
+        console.log(err);
       }
       
     };
