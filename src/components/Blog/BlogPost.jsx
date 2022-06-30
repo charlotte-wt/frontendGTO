135f6ca69b4dc7623b6335b650bc5a816cc8d60b
charlotte-wt <charlottewee01@gmail.com>
2022-07-01 00:09:26 +0800

    take from backend and now can edit & update (put) to backend
    left delete + write post

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,55 +1,77 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
-import { Link } from "react-router-dom";
-import { Context } from "../../context/Context";
+import AuthService from "../../services/auth.service";
+import BlogDataService from "../../services/blogService";
+import { Button } from "../Home/Button";
 import "./blogPost.css";
 
 export default function BlogPost() {
+
   const location = useLocation();
   const path = location.pathname.split("/")[2];
+
+
   const [post, setPost] = useState({});
-  const PF = "http://localhost:8000/images/";
-  const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [updateMode, setUpdateMode] = useState(false);
 
+
+  const user = AuthService.getCurrentUser();
+
   useEffect(() => {
     const getPost = async () => {
-      const res = await axios.get("http://localhost:8000/images/blogs/" + path);
+      const res = await BlogDataService.get(path);
       setPost(res.data);
       setTitle(res.data.title);
-      setDesc(res.data.desc);
+      setDesc(res.data.description);
     };
     getPost();
   }, [path]);
 
   const handleDelete = async () => {
     try {
-      await axios.delete(`/blogs/${post._id}`, {
+      await axios.delete(`/blogs/${post.id}`, {
         data: { username: user.username },
       });
       window.location.replace("/");
-    } catch (err) {}
+    } catch (err) { }
   };
 
   const handleUpdate = async () => {
     try {
-      await axios.put(`/blogs/${post._id}`, {
-        username: user.username,
-        title,
-        desc,
-      });
+
+      const data = {
+        title: title,
+        description: desc,
+        image: post.image,
+        userId: post.userId,
+        username: post.username
+      }
+
+      await BlogDataService.update(`update/${post.id}`, data);
       setUpdateMode(false)
-    } catch (err) {}
+    } catch (err) { }
   };
+
+  // user id is null fetching it will get error
+  // to check if person logged in is the blog poster 
+  // only blog poster can edit / update their blog post
+
+  var user_id = null;
+  try {
+    user_id = user.id;
+
+  } catch (err) {
+
+  }
 
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        {post.photo && (
-          <img src={PF + post.photo} alt="" className="singlePostImg" />
+        {post.image && (
+          <img src={post.image} alt="" className="singlePostImg" />
         )}
         {updateMode ? (
           <input
@@ -60,31 +82,44 @@ export default function BlogPost() {
             onChange={(e) => setTitle(e.target.value)}
           />
         ) : (
-          <h1 className="singlePostTitle">
-            {title}
-            {post.username === user?.username && (
+          <div className="singlePostTitle">
+
+
+            {post.userId === user_id && (
               <div className="singlePostEdit">
-                <i
-                  className="singlePostIcon far fa-edit"
+                <Button
+                  className='btns'
+                  buttonStyle='btn--pink'
+                  buttonSize='btn--middle'
                   onClick={() => setUpdateMode(true)}
-                ></i>
-                <i
-                  className="singlePostIcon far fa-trash-alt"
+
+                >Edit</Button>
+
+                <Button
+                  className='btns'
+                  buttonStyle='btn--pink'
+                  buttonSize='btn--middle'
                   onClick={handleDelete}
-                ></i>
+
+                >Delete</Button>
+
+
               </div>
             )}
-          </h1>
+            <h1 className="h1_title">{title}</h1>
+
+
+          </div>
         )}
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
             Author:
-            <Link to={`/?user=${post.username}`} className="link">
-              <b> {post.username}</b>
-            </Link>
+
+            <b> {post.username}</b>
+
           </span>
           <span className="singlePostDate">
-            {new Date(post.createdAt).toDateString()}
+            {/* {new Date(post.createdAt).toDateString()} */}
           </span>
         </div>
         {updateMode ? (
