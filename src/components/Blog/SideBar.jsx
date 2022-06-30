0f44a5b385b189e411f0c97679979f6a61567add
charlotte-wt <charlottewee01@gmail.com>
2022-07-01 00:11:19 +0800

    changed sidebar Links

--- src/components/Blog/SideBar.jsx
+++ src/components/Blog/SideBar.jsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarItem">
-        <span className="sidebarTitle">ABOUT ME</span>
+        <span className="sidebarTitle">SHARE ABOUT YOUR CAREER</span>
         <img
           src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
           alt=""
@@ -19,33 +19,33 @@ export default function Sidebar() {
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Life">
-              Life
+            <Link className="link" to="/blogs/technology">
+              Technology
             </Link>
           </li>
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Music">
-              Music
+            <Link className="link" to="/blogs/accountancy">
+              Accountancy
             </Link>
           </li>
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Sport">
-              Sport
+            <Link className="link" to="/blogs/startup">
+              Startups
             </Link>
           </li>
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Style">
-              Style
+            <Link className="link" to="/blogs/investmentbanking">
+              Investment Banking
             </Link>
           </li>
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Tech">
-              Tech
+            <Link className="link" to="/blogs/engineering">
+              Engineering
             </Link>
           </li>
           <li className="sidebarListItem">
-            <Link className="link" to="/posts?cat=Cinema">
-              Cinema
+            <Link className="link" to="/blogs/sports">
+              Sports
             </Link>
           </li>
         </ul>
